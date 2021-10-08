02afe0e6ac3c713bb1c8245a56d9d71faf92fbca
trabajoColegio <89045593+trabajoColegio@users.noreply.github.com>
2021-10-08 08:56:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ import loading from "./loading.js";
 import activateVideo from "./video.js";
 
 loading(".loading", ".intro-video")
-activateVideo("who we are.mp4", ".change1", "who i am.mp4", ".change2")
+activateVideo("who we are.mp4", ".change1", "who i am.mp4", ".change2", "the puzzle.mp4", ".change3")
